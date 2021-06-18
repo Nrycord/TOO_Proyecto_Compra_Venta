1bdcae2205d511c15116c38a0a503e86f7cc3f4f
Steve <rh18031@ues.edu.sv>
2021-06-18 16:16:30 -0600

    Arreglando vistas home

--- views/homeEmployee.php
+++ views/homeEmployee.php
@@ -17,40 +17,57 @@ if ($json != null) {
 
 ?>
 <main class="container">
-    <div class="row m-0 vh-100 px-3 py-5 row justify-content-center align-items-center">
-        <div class="col-md-5">
-            <div class="square-service-block shadow">
-                <a href="#">
-                    <div class="ssb-icon"><i class="fas fa-users" aria-hidden="true"></i></div>
-                    <h2 class="ssb-title"><strong>Clientes</strong></h2>
-                </a>
+    <div class="m-0 pt-5 vh-100 row justify-content-center align-items-center">
+        <div class="col-12 col-md-6">
+            <div class="container m-0 p-0">
+                <div class="row d-flex justify-content-start my-3">
+                    <img src="<?= BASE_DIR; ?>/assets/img/logo-lg.png" class="img-fluid" alt="logo-lg">
+                </div>
+                <div class="row d-flex text-left">
+                    <h1>Bienvenido <span class="badge badge-primary">nombre empleado</span></h1>
+                    <h4 class="my-1">Puedes acceder a las distintas opciones en el menú lateral</h4>
+                </div>
             </div>
         </div>
+        <div class="col-12 col-md-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-12">
+                        <div class="square-service-block shadow">
+                            <a href="#">
+                                <div class="ssb-icon"><i class="fas fa-users" aria-hidden="true"></i></div>
+                                <h2 class="ssb-title"><strong>Nueva Venta</strong></h2>
+                            </a>
+                        </div>
+                    </div>
 
-        <div class="col-md-5">
-            <div class="square-service-block shadow">
-                <a href="#">
-                    <div class="ssb-icon"> <i class="fas fa-paste" aria-hidden="true"></i> </div>
-                    <h2 class="ssb-title"><strong>Presupuestos</strong></h2>
-                </a>
-            </div>
-        </div>
+                    <div class="col-12 col-sm-6 col-md-12">
+                        <div class="square-service-block shadow">
+                            <a href="#">
+                                <div class="ssb-icon"> <i class="fas fa-truck" aria-hidden="true"></i> </div>
+                                <h2 class="ssb-title"><strong>Realizar Reporte</strong></h2>
+                            </a>
+                        </div>
+                    </div>
 
-        <div class="col-md-5">
-            <div class="square-service-block shadow">
-                <a href="#">
-                    <div class="ssb-icon"><i class="fas fa-archive" aria-hidden="true"></i></div>
-                    <h2 class="ssb-title"><strong>Productos</strong></h2>
-                </a>
-            </div>
-        </div>
+                    <div class="col-12 col-sm-6 col-md-12">
+                        <div class="square-service-block shadow">
+                            <a href="#">
+                                <div class="ssb-icon"><i class="fas fa-archive" aria-hidden="true"></i></div>
+                                <h2 class="ssb-title"><strong>Consultar Reporte</strong></h2>
+                            </a>
+                        </div>
+                    </div>
 
-        <div class="col-md-5">
-            <div class="square-service-block shadow">
-                <a href="#">
-                    <div class="ssb-icon"><i class="fas fa-file-medical-alt" aria-hidden="true"></i></div>
-                    <h2 class="ssb-title"><strong>Reportes</strong></h2>
-                </a>
+                    <div class="col-12 col-sm-6 col-md-12">
+                        <div class="square-service-block shadow">
+                            <a href="#">
+                                <div class="ssb-icon"><i class="fas fa-file-medical-alt" aria-hidden="true"></i></div>
+                                <h2 class="ssb-title"><strong>Nuevo Cliente</strong></h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
