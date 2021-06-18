966de6bad31dc0bee29c5a3338820468c175f27a
Nrycord <carlencordero@gmail.com>
2021-06-17 22:41:19 -0600

    agregando modelo clientes y productos

--- config/dbConfig.php
+++ config/dbConfig.php
@@ -10,9 +10,20 @@ define('DB_CHARSET', 'utf8');
 //Nombre de la tabla 1
 define('TBL_CLIENTES', '');
 //Campos de la tabla 1
+define('C_ID', '');
 define('C_NOMBRE', '');
 define('C_APELLIDO', '');
 define('C_DIR', '');
 define('C_DUI', '');
 define('C_TEL', '');
 define('C_TIPO', '');
+
+//Nombre de la tabla 2
+define('TBL_PRODUCTOS', '');
+//Campos de la tabla 2
+define('P_ID', '');
+define('P_NOMBRE', '');
+define('P_CANTIDAD', '');
+define('P_PRECIO', '');
+define('P_CATEGORIA', '');
+define('P_ID_PROV', '');
